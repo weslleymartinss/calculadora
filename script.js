a2beecbfd5eabe374454dd112f00b9c3458354f6
weslleymartinss <weslleymartins1@hotmail.com>
2024-06-20 00:08:57 -0300

    calculadora finalizada

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function inserir(num) {
-    document.getElementById('res').innerHTML= num;
-    
-     
+   let numero = document.getElementById('res').innerHTML;
+    document.getElementById('res').innerHTML = numero + num  
+}
+
+function clean() {
+    document.getElementById('res').innerHTML = ''
+}
+
+function back() {
+    let resultado = document.getElementById('res').innerHTML;
+    document.getElementById('res').innerHTML = resultado.substring(0, resultado.length -1);
+
+}
+function calcular() {
+    let resultado = document.getElementById('res').innerHTML;
+    if (resultado) {
+        document.getElementById('res').innerHTML = eval(resultado);
+    }
 }
